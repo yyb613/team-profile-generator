364f64e3ebad178a0d8d2d31b2cd2dc9bc77e53e
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-02-16 23:12:01 -0500

    created engineer and intern inquirer functions

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const employeeArr = [];
+const fs = require('fs');                   // fs
+const inquirer = require('inquirer');       // inquirer
+const Manager = require('./lib/Manager');   // Manager class
+const Engineer = require('./lib/Engineer'); // Engineer class
+const Intern = require('./lib/Intern');     // Intern class
+const employeeArr = [];                     // Employee array
 
 // Manager Questions
 const managerQuestions = [
     {
         type: 'input',
-        message: "What is the team manager's name?",
+        message: "What is the team manager's name?", // Name
         name: 'managerName'
     },
     {
         type: 'input',
-        message: "What is the team manager's id?",
+        message: "What is the team manager's ID?", // ID
         name: 'managerId'
     },
     {
         type: 'input',
-        message: "What is the team manager's email?",
+        message: "What is the team manager's email?", // Email
         name: 'managerEmail'
     },
     {
         type: 'input',
-        message: "What is the team manager's office number?",
+        message: "What is the team manager's office number?", // Office #
         name: 'managerOffice'
     },
 ];
@@ -33,31 +33,31 @@ const managerQuestions = [
 // Main Menu Question
 const menuQuestion = {
     type: 'list',
-    message: "Which type of team member would you like to add?",
+    message: "Which type of team member would you like to add?", // Choose type
     choices: ['Engineer', 'Intern', "I don't want to add any more team members"],
-    name: 'member-type'
+    name: 'memberType'
 }
 
 // Engineer Questions
 const engineerQuestions = [
     {
         type: 'input',
-        message: "What is your engineer's name?",
+        message: "What is your engineer's name?", // Name
         name: 'engineerName'
     },
     {
         type: 'input',
-        message: "What is your engineer's id?",
+        message: "What is your engineer's ID?", // ID
         name: 'engineerId'
     },
     {
         type: 'input',
-        message: "What is your engineer's email?",
+        message: "What is your engineer's email?", // Email
         name: 'engineerEmail'
     },
     {
         type: 'input',
-        message: "What is your engineer's GitHub username?",
+        message: "What is your engineer's GitHub username?", // GitHub
         name: 'engineerGit'
     }, 
 ];
@@ -66,22 +66,22 @@ const engineerQuestions = [
 const internQuestions = [
     {
         type: 'input',
-        message: "What is your intern's name?",
+        message: "What is your intern's name?", // Name
         name: 'internName'
     },
     {
         type: 'input',
-        message: "What is your intern's id?",
+        message: "What is your intern's ID?", // ID
         name: 'internId'
     },
     {
         type: 'input',
-        message: "What is your intern's email?",
+        message: "What is your intern's email?", // Email
         name: 'internEmail'
     },
     {
         type: 'input',
-        message: "What is your intern's school?",
+        message: "What is your intern's school?", // School
         name: 'internSchool'
     },
 ];
@@ -96,7 +96,7 @@ function writeToFile(fileName, data) {
 // Function to initialize app
 function init() {
     inquirer
-        .prompt(managerQuestions)
+        .prompt(managerQuestions) // ask Manager questions
         .then((answers) => {                             //--Variables--//
             const managerName = answers.managerName;     // Manager Name
             const managerId = answers.managerId;         // Manager ID
@@ -112,10 +112,46 @@ function init() {
 function mainMenu() {
     inquirer
         .prompt(menuQuestion)
-        .then(menuAnswer => {
-            // if()
+        .then(answer => {
+            if (answer.memberType === 'Engineer') {      // if choose Engineer
+                askEngineer();
+            } else if (answer.memberType === 'Intern') { // if choose Intern
+                askIntern();
+            } else {
+                writeToFile(); // 
+            }
         })
 }
+
+// Ask Engineer questions
+function askEngineer() {
+    inquirer
+        .prompt(engineerQuestions) // ask Engineer questions
+        .then((answers) => {                             //--Variables--//
+            const engineerName = answers.engineerName;   // Engineer Name
+            const engineerId = answers.engineerId;       // Engineer ID
+            const engineerEmail = answers.engineerEmail; // Engineer Email
+            const engineerGit = answers.engineerOffice;  // Engineer GitHub
+            const newEngineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGit); // create new Engineer
+            employeeArr.push(newEngineer) // push Engineer to Employee Array
+            mainMenu(); // prompt Main Menu
+        })
+};
+
+// Ask Intern questions
+function askIntern() {
+    inquirer
+        .prompt(internQuestions) // ask Intern questions
+        .then((answers) => {                             //--Variables--//
+            const internName = answers.engineerName;     // Intern Name
+            const internId = answers.engineerId;         // Intern ID
+            const internEmail = answers.engineerEmail;   // Intern Email
+            const internSchool = answers.engineerOffice; // Intern GitHub
+            const newIntern = new Intern(internName, internId, internEmail, internSchool); // create new Intern
+            employeeArr.push(newIntern) // push Intern to Employee Array
+            mainMenu(); // prompt Main Menu
+        })
+};
 
 // Function call to initialize app
 init();
